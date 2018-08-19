c3b8e69750758633c173f33c95c090f7e54bf875
vkill <vkill.net@gmail.com>
2018-08-19 10:21:17 +0800

    Update tests

--- Tests/DatadogStatsdTests/DatadogStatsdTests.swift
+++ Tests/DatadogStatsdTests/DatadogStatsdTests.swift
@@ -2,11 +2,46 @@ import XCTest
 @testable import DatadogStatsd
 
 final class DatadogStatsdTests: XCTestCase {
-    func testExample() {
-        XCTAssertEqual(1, 1)
+    func testServiceCheck() throws {
+        let host = "127.0.0.1"
+        let port = 8125
+        let conn = Connection(host, port)
+        let statsd = DatadogStatsd(host: host, port: port, connection: conn)
+
+        for _ in (1...10) {
+            let name = "test"
+            let status = DatadogStatsdServiceCheckMessageStatus.ok
+
+            // sends with only name and status
+            try statsd.serviceCheck(name, status)
+            XCTAssertEqual(conn.recv, ["_sc|\(name)|\(status.rawValue)"])
+            conn.cleanRecv()
+        }
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testServiceCheck", testServiceCheck),
     ]
+
+    class Connection: DatadogStatsdConnection {
+        let host: String
+        let port: Int
+        var recv: [String]
+
+        init(_ host: String, _ port: Int) {
+            self.host = host
+            self.port = port
+            self.recv = []
+        }
+
+        func cleanRecv() {
+            self.recv = []
+        }
+
+        public func write(_ text: String, to: (host: String, port: Int)) throws {
+            XCTAssertEqual(to.host, host)
+            XCTAssertEqual(to.port, port)
+            self.recv.append(text)
+        }
+    }
 }
