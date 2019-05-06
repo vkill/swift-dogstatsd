71c79412fcd11fb5073f652a0bd5b9516eade7aa
vkill <vkill.net@gmail.com>
2019-05-06 10:46:25 +0800

    Update example

--- Example/Sources/DatadogStatsdExample/main.swift
+++ Example/Sources/DatadogStatsdExample/main.swift
@@ -5,22 +5,18 @@ $ socat -v udp-recvfrom:8125,fork exec:'/bin/echo'
 
 import NIODatadogStatsd
 import DatadogStatsd
-import NIO
-import CoreFoundation
 
 let datadogStatsdConnection = NIODatadogStatsdConnection()
 _ = try datadogStatsdConnection.bind(
     host: "127.0.0.1",
     port: 29001,
-    on: MultiThreadedEventLoopGroup(numberOfThreads: 1)
-).wait()
-
-let datadogStatsd = DatadogStatsd(
-    host: "127.0.0.1",
-    port: 8125,
-    connection: datadogStatsdConnection
-)
-
-try datadogStatsd.count("count", 1)
-
-CFRunLoopRun()
+    on: nil
+).map { _ in
+    let datadogStatsd = DatadogStatsd(
+        host: "127.0.0.1",
+        port: 8125,
+        connection: datadogStatsdConnection
+    )
+    
+    try! datadogStatsd.count("count", 1)
+}.wait()
